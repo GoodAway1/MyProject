749c25bb34fa06ffff97ab19abdd007c1332548d
c_class <s@w>
2022-03-26 12:43:58 +0300

    MnForm

---
+++ MainForm.py
@@ -0,0 +1,10 @@
+from MyMainForm import Ui_MainWindow
+from PyQt5 import QtWidgets
+
+class MyMainForm(QtWidgets.QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.__ui = Ui_MainWindow
+        self.__ui.setupUi(self)
+        self.show()
